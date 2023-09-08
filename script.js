d0cfd7c25f162d48d1dbc69498c9bf1b7f50234e
kNaoreen <naoreen@greenspacehealth.com>
2023-09-07 23:12:45 -0700

    add exercises 1-3

--- script.js
+++ script.js
@@ -1,7 +1,13 @@
 addEventListener("DOMContentLoaded", (event) => {
-    document.getElementById("clickMeButton").onclick = function() {myFunction()};
+    document.getElementById("button1").onclick = function() {
+      updateCardImage("https://upload.wikimedia.org/wikipedia/commons/d/de/RWS_Tarot_01_Magician.jpg")
+    };
+
+    document.getElementById("button2").onclick = function() {
+      updateCardImage("https://upload.wikimedia.org/wikipedia/commons/8/88/RWS_Tarot_02_High_Priestess.jpg")
+    }
 });
 
-function myFunction() {
-  window.alert("Hello! You clicked this button :o")
-}
+// EXERCISE 3:
+// Write a function that updates the card image
+// Look at lines 3 and 7 for what the function should be called, and examples of what we'd expect to pass into the function (the parameter(s))
